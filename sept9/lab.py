70b4c3887c1a09eaaf2af24eb3f07ecbe3acf3c3
Hunter Hutchisson <hunter.hutchisson@gmail.com>
2021-09-10 16:44:37 -0500

    lab

--- sept9/lab.py
+++ sept9/lab.py
@@ -12,15 +12,15 @@
 # print(planets)
 
 
-HoustonCities = ["Katy", "Memorial City", "Sugar Land",
-                 "The Heights", "River Oaks", "Pasadena"]
-ClearLakeCities = ["League City", "Kemah", "Seabrook", "Webster", "El Lago"]
+# HoustonCities = ["Katy", "Memorial City", "Sugar Land",
+#                  "The Heights", "River Oaks", "Pasadena"]
+# ClearLakeCities = ["League City", "Kemah", "Seabrook", "Webster", "El Lago"]
 
-Houston = HoustonCities + ClearLakeCities
+# Houston = HoustonCities + ClearLakeCities
 
 # print(len(Houston))
 
-Houston.extend(["Pearland", "The Woodlands", "Deerpark"])
+# Houston.extend(["Pearland", "The Woodlands", "Deerpark"])
 
 # index = 0
 # while index < len(Houston):
@@ -29,35 +29,35 @@ Houston.extend(["Pearland", "The Woodlands", "Deerpark"])
 
 # print(len(Houston))
 
-htx1 = Houston[:4]
-htx2 = Houston[2:6]
-htx3 = Houston[-2:]
+# htx1 = Houston[:4]
+# htx2 = Houston[2:6]
+# htx3 = Houston[-2:]
 
-print(htx1)
-print(htx2)
-print(htx3)
+# print(htx1)
+# print(htx2)
+# print(htx3)
 
-Houston.insert(4, "Denver")
+# Houston.insert(4, "Denver")
 
-print(Houston)
+# print(Houston)
 
-Houston.pop()
+# Houston.pop()
 
-print(Houston)
+# print(Houston)
 
-print(Houston.index("Seabrook"))
+# print(Houston.index("Seabrook"))
 
-Houston.sort()
+# Houston.sort()
 
-print(Houston)
+# print(Houston)
 
-USCities = Houston.copy()
+# USCities = Houston.copy()
 
-print(USCities)
+# print(USCities)
 
-Houston.clear()
+# Houston.clear()
 
-print(Houston)
+# print(Houston)
 
 
 
@@ -74,12 +74,81 @@ print(Houston)
 
 # print(todos)
 
+# string = "DigitalCrafts"
+# rev = ""
+# index = len(string) -1
+# while index >=0:
+#     rev += string[index]
+#     index -=1
+# print(rev)
+# rng = range(10)
+# print(rng)
+
+# other_rng = range(6, 17, 2)
+
+# planets = ["Earth", "Jupiter", "Neptune", "Mars", "Saturn", "Mercury", "Uranus", "Venus"]
+# for planet in planets:
+#     print(planet)
+
+# for city in USCities:
+#     print(city)
+
+# for num in other_rng:
+#         print(num * 5 )
+
+# rng = range(1,11)
+# for num1 in rng:
+#     for num2 in rng:
+#         print(f"{num1} x {num2} = {num1 * num2}")
+#     print("...")
+
+# chapters = ["Python", "Javascript", "HTML/CSS", "Node", "React", "Redux"]
+# days = ["mon", "tue", "wed", "thur", "fri", "sat", "sun"]
+
+# for chapter in chapters:
+#     print(chapter)
+#     for day in days:
+#         print(day)
+
+# arr = [4, 2, 5, 7, 23, 6, 5]
+# sum = 0
+# for num in arr:
+#     sum += num
+# print(sum)
+
+# largest_number = arr[0]
+# for num in arr:
+#     if num > largest_number:
+#         largest_number = num
+# print(largest_number)
+
+#Fizz Buzz
 
 
 
-
-
-
+#three number sum
+a = [12, 3, 1, 2, -6, 5, -8, 6]
+a.sort()
+solution = []
+for i in range(0, (len(a) -1)):
+    l = i +1
+    r = len(a) - 1
+    while l < r:
+        ce = a[i]
+        left = a[l]
+        right = a[r]
+        if (ce + left + right == 0):
+            current_zero = []
+            current_zero.append(ce)
+            current_zero.append(left)
+            current_zero.append(right)
+            solution.append(current_zero)
+            l += 1
+        elif (ce + left + right > 0):
+            r -= 1
+        else:
+            l += 1
+print(solution)
 
 
 
