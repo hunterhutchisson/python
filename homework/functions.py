19521c869672c5322a4cf6823b4599211091b131
Hunter Hutchisson <hunter.hutchisson@gmail.com>
2021-09-13 15:48:05 -0500

    Homework Sept13

---
+++ homework/functions.py
@@ -0,0 +1,60 @@
+# 1. Find the smallest number
+
+# Write a function smallest that accepts a List of numbers as an argument.
+# It should return the smallest number in the List.
+
+# list1 = [10, 5, 15, 35, 1, 3, -1]
+# def smallest(lst):
+#     smallest_number = lst[0]
+#     for num in lst:
+#         if num < smallest_number:
+#             smallest_number = num
+#     return smallest_number
+
+# print(smallest(list1))
+
+# 2. Find the largest number
+
+# Write a function largest that accepts a List of numbers as an argument.
+# It should return the largest number in the List.
+
+# list1 = [10, 5, 15, 35, 1, 3, -1]
+# def largest(lst):
+#     largest_number = lst[0]
+#     for num in lst:
+#         if num > largest_number:
+#             largest_number = num
+#     return largest_number
+
+# print(largest(list1))
+
+# 3. Find the shortest String
+
+# Write a function shortest that accepts a List of Strings as an argument.
+# It should return the shortest String in the List.
+
+# list2 = ["7777777", "55555", "4444", "1", "999999999", "333", "22"]
+
+# def shortest(lst):
+#     shortest_string = lst[0]
+#     for string in lst:
+#         if len(shortest_string) > len(string):
+#             shortest_string = string
+#     return shortest_string
+
+# print(shortest(list2))
+
+# 4. Find the longest String
+
+# Write a function longest that accepts a List of Strings as an argument.
+# It should return the longest String in the List.
+
+# list2 = ["7777777", "55555", "4444", "1", "999999999", "333", "22"]
+# def longest(lst):
+#     longest_string = lst[0]
+#     for string in lst:
+#         if len(longest_string) < len(string):
+#             longest_string = string
+#     return longest_string
+
+# print(longest(list2))
